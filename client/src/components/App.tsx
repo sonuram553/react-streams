0d6a3a38dbab1c940e09ef57bec538cce1460c8f
Sonu Ram <sonuram6960@gmail.com>
2020-05-24 02:11:38 +0530

    Add Route tag for each stream components in our App component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,31 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to="/pagetwo">Go to PageTwo</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <Link to="/">Go to PageOne</Link>
-    </div>
-  );
-};
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
         </BrowserRouter>
       </div>
     );
