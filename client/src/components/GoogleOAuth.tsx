5043efa1753808b0dfcdb8585b678868ed8f4ab0
Sonu Ram <sonuram6960@gmail.com>
2020-06-01 00:39:07 +0530

    Add support for google OAuth, create new GoogleOAuth component

---
+++ client/src/components/GoogleOAuth.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+declare const G: any;
+
+class GoogleOAuth extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return <div>GoogleOAuth</div>;
+  }
+}
+
+export default GoogleOAuth;
